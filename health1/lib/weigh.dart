811c995be1678eb3660f4cce232aa4ff0f660da6
10746025 <dmc950213@gmail.com>
2021-11-05 14:52:20 +0800

    Add Datetime in weigh function

--- health1/lib/weigh.dart
+++ health1/lib/weigh.dart
@@ -1,38 +1,62 @@
 import 'package:flutter/material.dart';
 import "package:flutter/rendering.dart";
 import "package:syncfusion_flutter_charts/charts.dart";
-import 'package:intl/intl.dart';
-import "dart:developer";
+import 'package:date_time_picker/date_time_picker.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
+void main() => runApp(MyApp());
 
-void main() {
-  runApp(
-      MaterialApp(
-        home: weight(),
-      )
-  );
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter DateTimePicker Demo',
+      home: weight(),
+      localizationsDelegates: [
+        GlobalWidgetsLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [Locale('en', 'US')], //, Locale('pt', 'BR')],
+    );
+  }
 }
 class weight extends StatefulWidget {
   const weight({Key? key}) : super(key: key);
 
   @override
   _weightState createState() => _weightState();
+
 }
 class _weightState extends State<weight> {
   final List<String> names = <String>[];  //預先加入的資料集
   //final List<int> msgCount = <int>[2, 10];  //預先加入的資料集
   TextEditingController nameController = TextEditingController();//擷取文字用
 
-  late List<SalesData> _chartData;
+  GlobalKey<FormState> _oFormKey = GlobalKey<FormState>();
+  late TextEditingController _controller3;
+  String _valueChanged3 = '';
+  String _valueToValidate3 = '';
+  String _valueSaved3 = '';
+
+  late List<weightData> _chartData;
   late TooltipBehavior _tooltipBehavior;
 
   @override
   void initState(){
+    super.initState();
+    _controller3 = TextEditingController(text: DateTime.now().toString());
     _chartData = getChartData();
     _tooltipBehavior = TooltipBehavior(enable: true);
-    super.initState();
-  }
+    _getValue();
 
+  }
+  Future<void> _getValue() async {
+    await Future.delayed(const Duration(seconds: 3), () {
+      setState(() {
+      });
+    }); //並沒有實際功能
+  }
   void addItemToList(){
     setState(() {
       var Date = DateTime.now();
@@ -40,7 +64,7 @@ class _weightState extends State<weight> {
 
       names.insert(0,nameController.text);
 //      msgCount.insert(0, 0); //insert在0的位置加入0
-      _chartData.insert(_chartData.length, SalesData(Datemon.toDouble(),double.parse(nameController.text))); //圖表更新
+      _chartData.insert(_chartData.length, weightData(Datemon.toDouble(),double.parse(nameController.text))); //圖表更新
     });
   }
 
@@ -53,6 +77,28 @@ class _weightState extends State<weight> {
       ),
       body: Column(
           children: <Widget>[
+            SingleChildScrollView(
+              child: Form(
+                key: _oFormKey,
+                child: Column(
+                  children: <Widget>[
+                    DateTimePicker(
+                      type: DateTimePickerType.date,  //顯示格式
+                      dateMask: 'd MMM, yyyy',  //需求資料格式
+                      controller: _controller3,  //
+                      //initialValue: _initialValue,
+                      firstDate: DateTime(2000), //範圍
+                      lastDate: DateTime(2100),   //範圍
+                      icon: Icon(Icons.event),
+                      dateLabelText: 'Date',
+                      //locale: Locale('pt', 'BR'),
+                      selectableDayPredicate: (date) {
+                        return true;
+                      },
+                    ),],
+                ),
+              ),
+            ),
             Padding(  //用來設置Padding用的Widget
               padding: EdgeInsets.all(20),
               child: TextField(
@@ -63,6 +109,7 @@ class _weightState extends State<weight> {
                 ),
               ),
             ),
+
             RaisedButton(
               child: Text('加入'),
               onPressed: () {
@@ -95,11 +142,11 @@ class _weightState extends State<weight> {
               legend: Legend(isVisible: true), //顯示下面標籤
               tooltipBehavior: _tooltipBehavior,
               series: <ChartSeries>[
-                LineSeries<SalesData,double>(
+                LineSeries<weightData,double>(
                     name: 'Sales',  //改變標籤名稱
                     dataSource: _chartData,
-                    xValueMapper: (SalesData sales, _) => sales.year,  //X軸的資料
-                    yValueMapper: (SalesData sales, _) => sales.sales,  //Y軸的資料
+                    xValueMapper: (weightData sales, _) => sales.year,  //X軸的資料
+                    yValueMapper: (weightData sales, _) => sales.sales,  //Y軸的資料
                     dataLabelSettings: DataLabelSettings(isVisible: true),
                     enableTooltip: true  //final enable tooltip
                 )
@@ -114,18 +161,18 @@ class _weightState extends State<weight> {
       )
     );
   }
-  List<SalesData> getChartData(){
-    final List<SalesData> chartData = [
-      SalesData(7, 25),
-      SalesData(8, 12),
-      SalesData(9, 24),
+  List<weightData> getChartData(){
+    final List<weightData> chartData = [
+      weightData(7, 25),
+      weightData(8, 12),
+      weightData(9, 24),
     ];
     return chartData;
   }
 }
 
-class SalesData{
-  SalesData(this.year,this.sales);
+class weightData{
+  weightData(this.year,this.sales);
   final double year;
   final double sales;
 }
